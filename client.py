e7bc6c81ba6d9b64a0f7a0448facb5ccba46942b
Rigrey <ozhogin.m@gmail.com>
2024-11-01 05:19:56 +0300

    need to continue start_connection with changing nickname

--- client.py
+++ client.py
@@ -1,23 +1,89 @@
 import asyncio
+import argparse
+import sys
 
-async def tcp_echo_client():
-    reader, writer = await asyncio.open_connection("127.0.0.1", 12345)
+class ChatClient:
+    def __init__(self, server_host, server_port, nickname):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.nickname = nickname
 
-    print("Connected to the server. Type your messages below.")
+
+    async def start_connection(self):
+        reader, writer = await asyncio.open_connection(self.server_host, self.server_port)
+        writer.write(f"{self.nickname}".encode())
+        await writer.drain()
+        while True:
+            answer = (await reader.read(4096)).decode().strip()
+            if answer == "[ ! ] This nickname is already taken.":
+                print(answer+"\n", end = "")
+                while True:
+                    self.nickname = input('New nickname (16 chars max): ')
+                    if len(self.nickname) <= 16:
+                        writer.write(f"{self.nickname}".encode())
+                        await writer.drain()
+                        break
+                    print("Nickname is too long.")
+
+            elif answer == f"[ ! ] {self.nickname} joined the chat!":
+                print(answer)
+                break
+            else:
+                print("Unexpected response from server.")
+                writer.close()
+                await writer.wait_closed()
+                sys.exit(1)
+
+        await asyncio.gather(
+            self.listen_for_messages(reader),
+            self.send_messages(writer)
+        )
+        writer.close()
+        await writer.wait_closed()
+        sys.exit(0)
+
+
+    async def listen_for_messages(self, reader):
+        while True:
+            data = await reader.read(4096)
+            if not data:
+                print("\nConnection closed")
+                break
+            print(f"{data.decode()}\n", end="")
+
+
+    async def send_messages(self, writer):
+        while True:
+            message = await asyncio.get_event_loop().run_in_executor(None, input)
+            if message.lower() == "disconnect":
+                writer.write(message.encode())
+                await writer.drain()
+                break
+            writer.write(message.encode())
+            await writer.drain()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Клиент для временно централизованного мессенджера"
+    )
+    parser.add_argument("--cmd", type = bool, default = False, help = "Запуск через параметры или нет")
+    parser.add_argument("--host", type = str, default = "127.0.0.1", help = "Адрес для сервера")
+    parser.add_argument("--port", type = str, default = "12345", help = "Порт для сервера")
+    args = parser.parse_args()
+
+    if args.cmd:
+        server_host = args.host
+        server_port = args.port
+    else:
+        server_host = input('Server host: ')
+        server_port = input('Server port: ')
 
     while True:
-        message = input("Client: ")
-        writer.write(message.encode())
-        await writer.drain()
-
-        data = await reader.read(100)
-        print(f"Server: {data.decode()}")
-
-        if message.lower() == "exit":
-            print("Closing the connection.")
+        nickname = input('Nickname (16 chars max): ')
+        if len(nickname) <= 16:
             break
+        print("Nickname is too long.")
 
-    writer.close()
-    await writer.wait_closed()
-
-asyncio.run(tcp_echo_client())
+    client = ChatClient(server_host, server_port, nickname)
+    asyncio.run(client.start_connection())
--- server.py
+++ server.py
@@ -1,30 +1,62 @@
+import argparse
 import asyncio
+from turtledemo.clock import datum
 
-async def handle_client(reader, writer):
-    address = writer.get_extra_info('peername')
-    print(f"Connected to {address}")
 
-    while True:
-        data = await reader.read(100)
-        message = data.decode()
+class ChatServer():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.clients = {}
 
-        if not data:
-            print(f"Connection closed by {address}")
-            break
 
-        print(f"Received from client: {message}")
-        writer.write(data)
-        await writer.drain()
+    async def handle_client(self, reader, writer):
+        nickname = (await reader.read(16)).decode().strip()
+        if nickname in self.clients.values():
+            writer.write("[ ! ] This nickname is already taken.".encode())
+            await writer.drain()
+        else:
+            self.clients[writer] = nickname
+            await self.broadcast(f"[ ! ] {nickname} joined the chat!")
+            try:
+                while True:
+                    data = await reader.read(4096)
+                    message = data.decode().strip()
+                    if not message or message == 'disconnect':
+                        break
+                    await self.broadcast(f"{nickname}: {message}")
+            finally:
+                await self.disconnect_client(writer)
 
-    writer.close()
-    await writer.wait_closed()
 
-async def start_server():
-    server = await asyncio.start_server(handle_client, "127.0.0.1", 12345)
-    addr = server.sockets[0].getsockname()
-    print(f"Server started on {addr}")
+    async def disconnect_client(self, writer):
+        nickname = self.clients.pop(writer)
+        await self.broadcast(f"[ ! ] {nickname} left the chat!")
+        writer.close()
+        await writer.wait_closed()
 
-    async with server:
-        await server.serve_forever()
 
-asyncio.run(start_server())
+    async def broadcast(self, message):
+        for writer in self.clients.keys():
+            writer.write(message.encode())
+            await writer.drain()
+
+
+
+    async def start_server(self):
+        server = await asyncio.start_server(self.handle_client, self.host, self.port)
+        print(f"Server running on {self.host}:{self.port}")
+
+        async with server:
+            await server.serve_forever()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Сервер для временно централизованного мессенджера"
+    )
+    parser.add_argument("--host", type = str, default = "127.0.0.1", help = "Адрес для сервера")
+    parser.add_argument("--port", type = str, default = "12345", help = "Порт для сервера")
+    args = parser.parse_args()
+    chat_server = ChatServer(args.host, args.port)
+    asyncio.run(chat_server.start_server())
