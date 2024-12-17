201cf1ee541f74b5bdfe21d9f91bff876eac23e2
Rigrey <ozhogin.m@gmail.com>
2024-12-17 08:44:36 +0300

    Added colorful output for diff users. User is hash now.  Enum class for command handler. New header for the client class and also correct me and parent definition.  Receiving messages uses new function (with some mistakes, but its fine...). Rewrite of unpack and pack functions for new Header. Also added hashing ip and port to connection handler for assigning hash for new connection

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
 import argparse
 import asyncio
-import string
 import struct
 import sys
-#TODO: DO NEW SERVER
+from enum import Enum
+
+
+def number_to_rgb(number):
+    number = int(number)
+    r = (number * 123) % 256
+    g = (number * 321) % 256
+    b = (number * 213) % 256
+    return r, g, b
+
 
 def find_free_port():
     from socket import socket, AF_INET, SOCK_STREAM
@@ -13,19 +21,28 @@ def find_free_port():
     temp_socket.close()
     return port
 
+
 def get_my_ip():
     import psutil
     from socket import AddressFamily
     for interface, addrs in psutil.net_if_addrs().items():
-        if "Radmin VPN" in interface: # I temporarily use Radmin VPN
+        if "Radmin VPN" in interface:  # I temporarily use Radmin VPN
             for addr in addrs:
                 if addr.family == AddressFamily.AF_INET:
                     return addr.address
     return None
 
 
+class Command(Enum):
+    PRINT_MESSAGE = 0
+    DISCONNECTED_MESSAGE = 1
+    CONNECTED_MESSAGE = 2
+    GIVE_ID = 3
+    HANDLE_NEW_CONNECTION = 4
+
+
 class User_Connection:
-    def __init__(self, host, port, reader = None, writer = None):
+    def __init__(self, host, port, reader=None, writer=None):
         self.host = host
         self.port = port
         self.reader = reader
@@ -34,152 +51,159 @@ class User_Connection:
     def __str__(self):
         return f"{self.host}:{self.port}"
 
+
 class ChatClient:
     def __init__(self, server_host, server_port):
         self.parent = User_Connection(server_host, server_port)
         self.me = User_Connection(get_my_ip(), find_free_port())
         self.child = None
         self.nickname = ''
-        self.format = "<BH"
+        self.header = "<BHH"
         self.nicknames = set()
         self.command_handlers = {
-            0: self.print_message,
-            1: self.disconnected_message,
-            2: self.connected_message,
-            3: self.update_nicknames,
-            4: self.handle_new_connection,
+            Command.PRINT_MESSAGE: self.print_message,
+            Command.DISCONNECTED_MESSAGE: self.disconnected_message,
+            Command.CONNECTED_MESSAGE: self.connected_message,
+            Command.GIVE_ID: self.get_id,
+            Command.HANDLE_NEW_CONNECTION: self.handle_new_connection,
         }
 
-#FIXME: CHILD?
     async def start_connection(self):
         try:
-            print(2)
-            if self.parent.host == 'localhost':
-                await self.set_nickname()
+            if self.parent.host is None:
                 await self.server_startup()
+                self.nickname = str(hash(str(self.me.host) + ":" + str(self.me.host)) % 10 ** 9)
                 print(f"New person can join by this: {self.me}")
             else:
-                print(3)
-                self.me.reader, self.me.writer = await asyncio.open_connection(self.parent.host, self.parent.port)
-                print(4)
-                await self.unpack_message(await self.me.reader.read(65538))
-                print(5)
-                await self.set_nickname()
-                print(6)
+                self.parent.reader, self.parent.writer = await asyncio.open_connection(self.parent.host,
+                                                                                       self.parent.port)
+                header = await asyncio.wait_for(self.parent.reader.readexactly(struct.calcsize(self.header)), timeout=5)
+                if not header:
+                    raise Exception("Connection failed on header part")
+                command, length, nickname_length = struct.unpack(self.header, header)
+                body_data = await asyncio.wait_for(self.parent.reader.readexactly(length), timeout=5)
+                if not body_data:
+                    raise Exception("Connection on body part")
+                full_message = header + body_data
+                command, nickname, message = await self.unpack_message(full_message)
+                await self.command_handlers[Command(command)](nickname, message)
                 await self.server_startup()
-                data = await self.pack_message(2, self.nickname)
-                await self.broadcast_message(data, self.me.writer)
+                data = await self.pack_message(Command.CONNECTED_MESSAGE.value, self.nickname)
+                await self.send_message(data, self.parent.writer)
             await asyncio.gather(
                 self.receive_messages(),
-                self.send_messages(),
+                self.process_input(),
             )
         except ConnectionError as e:
             print(f'Connection failed: {e}')
         finally:
-            if self.me.writer:
-                self.me.writer.close()
-                await self.me.writer.wait_closed()
+            if self.parent.writer:
+                self.parent.writer.close()
+                await self.parent.writer.wait_closed()
             sys.exit(0)
 
-# FIXME: RECEIVE-FUNC TRANSFORM TO BROADCAST-FUNC / SEND TO BROADCAST-FUNC
     async def receive_messages(self):
         while True:
             try:
-                if self.child:
-                    child_data = await self.child.reader.read(65538)
-                    if child_data:
-                        await self.unpack_message(child_data)
-                        await self.broadcast_message(child_data, self.me.writer)
-                if self.me.reader:
-                    parent_data = await self.me.reader.read(65538)
-                    if parent_data:
-                        await self.unpack_message(parent_data)
-                        await self.broadcast_message(parent_data, self.child.writer)
+                if self.child and self.child.reader:
+                    await self.process_stream(self.child.reader, self.parent.writer)
+
+                if self.me and self.parent.reader:
+                    await self.process_stream(self.parent.reader, self.child.writer if self.child else None)
+
+                await asyncio.sleep(0)
+            except asyncio.TimeoutError:
+                pass
             except Exception as e:
                 print(f"Failed to receive message: {e}")
                 pass
 
-    async def send_messages(self):
+    # FIXME: Problem with Russian.
+    async def process_stream(self, reader, writer):
+        while True:
+            header = await asyncio.wait_for(reader.readexactly(struct.calcsize(self.header)), timeout=5)
+            if not header:
+                break
+            command, length, nickname_length = struct.unpack(self.header, header)
+            body_data = await asyncio.wait_for(reader.readexactly(length), timeout=5)
+            if not body_data:
+                break
+            full_message = header + body_data
+            command, nickname, message = await self.unpack_message(full_message)
+            await self.command_handlers[Command(command)](nickname, message)
+            if writer:
+                await self.send_message(full_message, writer)
+            await asyncio.sleep(0)
+
+    async def process_input(self):
         while True:
             message = await asyncio.to_thread(input)
-            if message.lower() == "disconnect": # FIXME: THIS SHOULD GIVE INFO ABOUT CONNECTIONS. LISTENER SHOULD CONNECT BOTH
-                data_child = await self.pack_message(1, f'{self.nickname};{self.me}')
-                data_parent = await self.pack_message(1, self.nickname)
-                await self.broadcast_message(data_child, self.child.writer)
-                await self.broadcast_message(data_parent, self.me.writer)
+            if message.lower() == "!disconnect":  # TODO: THIS SHOULD GIVE INFO ABOUT CONNECTIONS
+                data_child = await self.pack_message(Command.DISCONNECTED_MESSAGE.value, f'{self.nickname};{self.me}')
+                data_parent = await self.pack_message(Command.DISCONNECTED_MESSAGE.value, self.nickname)
+                await self.send_message(data_child, self.child.writer)
+                await self.send_message(data_parent, self.parent.writer)
                 break
-            elif message.lower() == "new_connection":
+            elif message.lower() == "!new_connection":
                 if not self.child:
-                    print(f"You can connect new user by this data: {message.split(';')[0]}")
+                    print(f"You can connect new user by this data: {self.me}")
                     continue
-                data = await self.pack_message(4, str(self.me))
-                await self.broadcast_message(data, self.child.writer) # FIXME : NOT DONE YET. ADD THIS COMMAND TO COMMAND_HANDLER + ADD NEW FUNC
+                data = await self.pack_message(Command.HANDLE_NEW_CONNECTION.value, str(self.me))
+                await self.send_message(data, self.child.writer)
                 continue
-            if self.me.writer or self.child:
-                data = await self.pack_message(4, message)
-            if self.me.writer:
-                await self.broadcast_message(data, self.me.writer)
+            if self.parent.writer or self.child:
+                data = await self.pack_message(Command.PRINT_MESSAGE.value, message)
+            if self.parent.writer:
+                await self.send_message(data, self.parent.writer)
             if self.child:
-                await self.broadcast_message(data, self.child.writer)
+                await self.send_message(data, self.child.writer)
 
-    async def broadcast_message(self, data, writer):
+    async def send_message(self, data, writer):
         if writer:
             writer.write(data)
             await writer.drain()
 
     async def pack_message(self, command, message):
-        return struct.pack(self.format + f"{len(message)}s", command, len(message), message.strip().encode())
+        message = message.strip()
+        return struct.pack(self.header + f"{len(self.nickname)}s{len(message)}s", command, len(self.nickname + message),
+                           len(self.nickname), self.nickname.encode(), message.encode())
 
     async def unpack_message(self, data):
         try:
-            format_size = struct.calcsize(self.format)
-            if len(data) < format_size:
-                raise ValueError("Incomplete message header.")
-            command, length = struct.unpack(self.format, data[:format_size])
-            if command not in self.command_handlers:
-                raise ValueError("Unknown command received.")
-
-            message = data[format_size:format_size + length].decode()
-            await self.command_handlers[command](message)
+            header_size = struct.calcsize(self.header)
+            if len(data) < header_size:
+                raise ValueError("Incomplete message header")
+            command, data_length, nickname_length = struct.unpack(self.header, data[:header_size])
+            if Command(int(command)) not in self.command_handlers:
+                raise ValueError("Unknown command received")
+            if len(data[header_size:]) < data_length:
+                raise ValueError("Incomplete message payload")
+            unpack_format = f"{nickname_length}s{data_length - nickname_length}s"
+            nickname, message = struct.unpack(unpack_format, data[header_size:header_size + data_length])
+            nickname = nickname.decode()
+            message = message.decode()
+            return int(command), nickname, message
         except Exception as e:
             print(f"Failed to unpack message: {e}")
 
+    async def print_message(self, nickname, message):
+        r, g, b = number_to_rgb(nickname)
+        print(f"\033[38;2;{r};{g};{b}m{nickname}\033[0m: {message}")
 
-    async def set_nickname(self):
-        while True:
-            nickname = input('New Nickname (16 chars max): ').strip()
-            if len(nickname) == 0:
-                print("Nickname cannot be empty.")
-            elif nickname in self.nicknames:
-                print(f'Nickname {nickname} already set! Choose another one.')
-            elif len(nickname) > 16:
-                print("Nickname is too long.")
-            elif any(char in string.punctuation for char in nickname):
-                print("Nickname contains punctuation.")
-            else:
-                self.nickname = nickname
-                self.nicknames.add(nickname)
-                break
-
-    async def print_message(self, message):
-        print(message)
-
-#FIXME: THIS SHIT DON'T RECONNECT AFTER DISCONNECTION
-    async def disconnected_message(self, message):
+    # TODO: Reconnection of users (child to parent)
+    async def disconnected_message(self, nickname, message):
         print(f"[ ! ] {message} left the chat!")
-        self.nicknames.discard(message)
 
-    async def connected_message(self, message):
+    async def connected_message(self, nickname, message):
         print(f"[ ! ] {message} joined the chat!")
-        self.nicknames.add(message)
 
-    async def update_nicknames(self, message):
-        self.nicknames.update(message.split(', '))
+    async def get_id(self, nickname, message):
+        self.nickname = message
 
-    async def handle_new_connection(self, message): #TODO: CONTINUE WORKING ON THIS FUNCTION
+    async def handle_new_connection(self, nickname, message):
         if not self.child:
-            data = await self.pack_message(4, f'{self.me};{message}')
-            await self.broadcast_message(data, self.me.writer)
+            data = await self.pack_message(Command.HANDLE_NEW_CONNECTION.value, f'{self.me};{message}')
+            await self.send_message(data, self.parent.writer)
         elif message.split(';')[1] == str(self.me):
             print(f"You can connect new user by this data: {message.split(';')[0]}")
 
@@ -194,35 +218,30 @@ class ChatClient:
             await server.serve_forever()
 
     async def handle_connection(self, reader, writer):
-        print(10)
         try:
             if self.child:
                 writer.close()
                 await writer.closed()
                 raise Exception("This node has max amount of connections.")
             peer = writer.get_extra_info('peername')
-            print(11)
             self.child = User_Connection(peer[0], peer[1], reader, writer)
-            print(12)
-            data = await self.pack_message(3, str(self.nicknames)[1:-1])
-            print(13)
-            await self.broadcast_message(data, self.child.writer)
-            print(14)
+            data = await self.pack_message(Command.GIVE_ID.value,
+                                           str(hash(str(peer[0]) + ":" + str(peer[1])) % 10 ** 9))
+            await self.send_message(data, self.child.writer)
         except Exception as e:
             print(f"Failed to handle new connection: {e}")
 
-# TODO: ADD LOCALHOST AS START
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=' Client for temporarily (De)centralized Messenger'
     )
     parser.add_argument('-c', '--cmd', type=bool, default=False, help='Run through command line with parameters')
-    parser.add_argument('-s', '--server', type=str, default='localhost', help='Server host')
+    parser.add_argument('-s', '--server', type=str, default=None, help='Server host')
     parser.add_argument('-p', '--port', type=str, default=f'{find_free_port()}', help='Server port')
     args = parser.parse_args()
 
     server_host = args.server if args.cmd else input('Server host: ')
     server_port = args.port if args.cmd else input('Server port: ')
-    print(1)
     client = ChatClient(server_host, server_port)
     asyncio.run(client.start_connection())
--- test.py
+++ test.py
@@ -1,29 +1,47 @@
-import argparse
-import string
-# import struct
-#
-# FORMAT = "<BH"
-#
-# def pack_message(message):
-#     return struct.pack(FORMAT+f"{len(message)}s", 0, len(message), message.encode())
-#
-# def unpack_message(data):
-#     format_size = struct.calcsize(FORMAT)
-#     if len(data) < format_size:
-#         return data.decode()
-#     identifier, length = struct.unpack(FORMAT, data[:format_size])
-#     if len(data) < format_size + length:
-#         message = data[format_size:]
-#     else:
-#         message = struct.unpack(f"<{length}s", data[format_size:format_size + length])[0]
-#     return message.decode()
-#
-# data = pack_message(65535*"a")
-# print(data)
-# size = struct.calcsize(FORMAT + f"{len(data)-struct.calcsize(FORMAT)}s")
-# print(size)
+import struct
+from struct import unpack
 
+processed_hashs = set()
+peer = {}
+peer[0] = "192.168.0.0"
+peer[1] = "21234"
 
+def number_to_rgb(number):
+    # Ensure number is non-negative
+    number = abs(number)
+    # Map the number to a color
+    r = (number * 123) % 256  # Vary red component
+    g = (number * 321) % 256  # Vary green component
+    b = (number * 213) % 256  # Vary blue component
+    print(f"\033[38;2;{r};{g};{b}m{(r, g, b)}\033[0m")
 
-# if any(char in string.punctuation for char in "asb,sadad"):
-#     print("TRUE")
+new_hash = hash(str(peer[0])+":"+str(peer[1])) % 10 ** 9
+number_to_rgb(new_hash)
+
+header = "<BHH"
+nickname = "123456789"
+def unpack_message(data):
+    try:
+        header_size = struct.calcsize(header)
+        if len(data) < header_size:
+            raise ValueError("Incomplete message header")
+        command, data_length, nickname_length = struct.unpack(header, data[:header_size])
+        if len(data[header_size:]) < data_length:
+            raise ValueError("Incomplete message payload")
+        unpack_format = f"{nickname_length}s{data_length - nickname_length}s"
+        nickname, message = struct.unpack(unpack_format, data[header_size:header_size + data_length])
+        nickname = nickname.decode()
+        message = message.decode()
+        return int(command), nickname, message
+    except Exception as e:
+        print(f"Failed to unpack message: {e}")
+
+def pack_message(command, message):
+    message = message.strip()
+    return struct.pack(header + f"{len(nickname)}s{len(message)}s", command, len(nickname + message), len(nickname), nickname.encode(), message.encode())
+
+packed = pack_message(0, "you're gay")
+print(packed)
+
+unpacked = unpack_message(packed)
+print(unpacked)
